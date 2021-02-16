a08459f07092560977b5a638e8eadf9ba6136157
michael.zikes <michael.zikes@intauria.com>
2021-02-16 14:37:22 +0100

    Nx cache

--- apps/luggage/src/app/app.component.ts
+++ apps/luggage/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'luggage';
+
+  // He was here as well! :|
 }
--- libs/luggage/domain/src/lib/application/checkin.facade.ts
+++ libs/luggage/domain/src/lib/application/checkin.facade.ts
@@ -8,10 +8,11 @@ import { LuggageDataService } from '../infrastructure/luggage.data.service';
 @Injectable({ providedIn: 'root' })
 export class CheckinFacade {
 
-    private luggageListSubject = new BehaviorSubject<Luggage[]>([]); 
+    private luggageListSubject = new BehaviorSubject<Luggage[]>([]);
     luggageList$ = this.luggageListSubject.asObservable();
 
     constructor(private luggageDataService: LuggageDataService) {
+      // Michael was here! :)
     }
 
     load(): void {
